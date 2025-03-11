ee42104b0193238a84f16b79411d0a0cd9bb8742
Gatt101 <gauravpatilk11@gmail.com>
2025-03-11 21:31:43 +0530

    final error fix

--- src/main/java/Ecommerce/demo/config/SecurityConfig.java
+++ src/main/java/Ecommerce/demo/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://ecommerce-backend-6pfd.onrender.com")); // Add your allowed origins here
+        configuration.setAllowedOrigins(Arrays.asList("https://e-commerce-alpha-five-96.vercel.app")); // Add your allowed origins here
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
